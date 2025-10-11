e8e87e6658870a5699176c26686baa8567f13279
hitesh2114-exe <hiteshpandey2114@gmail.com>
2025-10-11 14:53:16 +0530

    changes

--- frontend/src/LandingPage/SignUp/SignUp.jsx
+++ frontend/src/LandingPage/SignUp/SignUp.jsx
@@ -1,11 +1,6 @@
 import React from 'react';
 import OpenFreeDemate from './OpenFreeDemate';
 import SignUpNow from './SignUpNow';
-import InvestmentOptions from './InvestmentOptions';
-import Steps from './Steps';
-import Benefits from './Benefits';
-import Explore from './Explore';
-import FAQs from './FAQs';
 
 function SignUp() {
     return (
