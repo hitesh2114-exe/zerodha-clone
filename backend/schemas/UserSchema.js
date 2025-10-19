af716dc6de5cd2c596a998a7d91019a06a56e9a5
hitesh2114-exe <hiteshpandey2114@gmail.com>
2025-10-19 19:21:51 +0530

    changes made

--- backend/schemas/UserSchema.js
+++ backend/schemas/UserSchema.js
@@ -4,6 +4,18 @@ const UserSchema = new Schema({
   username: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
+  holdings : {
+    type : [
+      {
+        name: { type: String, required: true },     // Stock name or symbol
+        qty: { type: Number, required: true },      // Quantity purchased
+        avg: { type: Number, required: true },      // Average purchase price
+        price: { type: Number, required: true },    // Current price
+        net: { type: String },                      // Net gain/loss (can be computed)
+        day: { type: String }                       // Daily change (can be computed)
+      }
+    ], default: []
+  }
 });
 
 module.exports = { UserSchema };
