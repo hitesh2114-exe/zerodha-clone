2eb32f0333d27f832d84a125618b2a07e3e7ef9f
hitesh2114-exe <hiteshpandey2114@gmail.com>
2025-10-19 23:07:04 +0530

    changes made

--- dashboard/src/components/Holdings.jsx
+++ dashboard/src/components/Holdings.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useState, useEffect, useContext } from "react";
 import axios from "axios";
 import { VerticalGraph } from "./VerticalGraph";
-
+import GeneralContext from "./GeneralContext";
 
 const Holdings = () => {
   const [holdings, setHoldings] = useState([]);
