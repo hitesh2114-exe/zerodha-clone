f7cd25fd5aa03706cb880306dc66864d368550ad
hitesh2114-exe <hiteshpandey2114@gmail.com>
2026-02-19 19:47:14 +0530

    changes made

--- dashboard/src/components/Home.jsx
+++ dashboard/src/components/Home.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ProtectedRoute from "../ProtectedRoute";
 import Dashboard from "./Dashboard";
 import TopBar from "./TopBar";
-import ProtectedRoute from "../ProtectedRoute";
 
 const Home = () => {
   return (
